283fa5df4f05b4d45a682af18180ef105d026f40
Austin Burk <apburk@gmail.com>
2022-07-12 12:26:58 -0400

    Add some more app versions, switch away from gplaycli

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # sudofox/hatena-app-archive
 
 An archive of all versions of any and all mobile apps created by Hatena
+
+# Requirements
+
+- [googleplay](https://github.com/89z/googleplay)
+  - You can use a [Google App Password](https://myaccount.google.com/apppasswords) instead of your main Google account password
+- aapt (via your favorite package manager)
+
+
--- scripts/download.sh
+++ scripts/download.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# download android package with gplaycli
+# download android package with googleplay
 # Usage: ./download.sh <package_name> [optional version]
 
-if [ $# -ne 1 ]; then
-    echo "Usage: ./download.sh <package_name> [optional version]"
+if [ $# -ne 2 ]; then
+    echo "Usage: ./download.sh <package_name> <numeric version code>"
     exit 1
 fi
 
+PACKAGE=$1
+VERSION=$2
+
 # fixes for bugs (existing as of 2021-08-17)
-# https://github.com/matlink/gplaycli/issues/260#issuecomment-778663387
 # https://github.com/NoMore201/googleplay-api/issues/132#issuecomment-671605973
 
-python -m gplaycli -c ./gplaycli.conf -av -p -v -d $1
+googleplay -a $PACKAGE -v $2
